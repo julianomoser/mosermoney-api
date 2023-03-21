63712f242051b3cec0719964070d2c3ec03997c8
Juliano Moser <juliano.moserjj@gmail.com>
2023-03-21 13:49:41 -0300

    first commit

---
+++ src/main/java/br/com/moser/mosermoney/MosermoneyApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.moser.mosermoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosermoneyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MosermoneyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/moser/mosermoney/MosermoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moser.mosermoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosermoneyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
