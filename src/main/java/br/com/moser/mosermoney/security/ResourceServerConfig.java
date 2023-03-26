767d3e8bb83dc7fb8e8ef0265eb15cae260fc934
Juliano Moser <juliano.moserjj@gmail.com>
2023-03-26 00:37:03 -0300

    Implementado segurança com OAuth 2 e Password Flow

---
+++ src/main/java/br/com/moser/mosermoney/security/ResourceServerConfig.java
@@ -0,0 +1,55 @@
+package br.com.moser.mosermoney.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+/**
+ * @author Juliano Moser
+ */
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin@mosermoney.com")
+                .password("admin")
+                .roles("ROLE");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/categorias").permitAll()
+                .anyRequest().authenticated()
+            .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .csrf().disable()
+                .oauth2ResourceServer().opaqueToken();
+    }
+
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
