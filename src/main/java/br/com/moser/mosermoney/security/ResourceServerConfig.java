60959e62b2626b5c00cdfd32d83f9f46e316c146
Juliano Moser <juliano.moserjj@gmail.com>
2023-03-26 23:35:14 -0300

    Renovando o access token com o refresh token

--- src/main/java/br/com/moser/mosermoney/security/ResourceServerConfig.java
+++ src/main/java/br/com/moser/mosermoney/security/ResourceServerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -71,6 +72,12 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
         return NoOpPasswordEncoder.getInstance();
     }
 
+    @Bean
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
     private JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
@@ -92,4 +99,5 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
         return jwtAuthenticationConverter;
     }
+
 }
